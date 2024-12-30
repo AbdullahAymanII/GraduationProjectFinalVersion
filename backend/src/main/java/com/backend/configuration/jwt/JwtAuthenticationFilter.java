983c2960b76d5df03c6b0659a9ba8e2d574e1432
Abdullah Ayman <abdayman1092002@gmail.com>
2024-12-30 23:49:32 +0300

    ;

--- backend/src/main/java/com/backend/configuration/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/backend/configuration/jwt/JwtAuthenticationFilter.java
@@ -21,7 +21,6 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
         this.jwtUtil = jwtUtil;
     }
 
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
