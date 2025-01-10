dcc93cbdce2cba4f4c66407c19f45162d34751af
Abdullah Ayman <abdayman1092002@gmail.com>
2025-01-11 00:16:24 +0300

    ML_UPDATED v5

--- MLModel/app.py
+++ MLModel/app.py
@@ -58,6 +58,77 @@
 #
 
 
+#
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# import pandas as pd
+# import joblib
+# import pickle
+# import xgboost as xgb
+# from PreprocessTrain import PreprocessTrain
+# from Stages import *
+#
+# app = Flask(__name__)
+# CORS(app)
+#
+# # Load models
+# CIC_Binary = joblib.load("models/binary.pkl")
+# CIC_Multi = joblib.load("models/multi.pkl")
+# with open('models/pipeline.pkl', 'rb') as file:
+#     pipeline = pickle.load(file)
+#
+#
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     if 'file' not in request.files:
+#         return jsonify({"error": "No file part in the request"}), 400
+#
+#     if CIC_Binary is None or CIC_Multi is None:
+#         return jsonify({"error": "Models not loaded"}), 500
+#
+#     file = request.files['file']
+#
+#     try:
+#         data = pd.read_csv(file)
+#         print(data.shape)
+#         binary, multi= pipeline.fit_test(data)
+#         print(binary.shape)
+#         print(multi.shape)
+#
+#         binary_dmatrix = xgb.DMatrix(binary)
+#         binary_predictions = CIC_Binary.predict(binary_dmatrix)
+#
+#         if any(binary_predictions):
+#             return jsonify({"message": "No attacks occurred"})
+#
+#         attack_indices = [i for i, pred in enumerate(binary_predictions) if pred == 0]
+#         attack_data = multi.iloc[attack_indices]
+#         multi_dmatrix = xgb.DMatrix(attack_data)
+#         multi_predictions = CIC_Multi.predict(multi_dmatrix)
+#
+#         print(multi_predictions)
+#
+#         binaryLables = pipeline.getBinaryLabelMapping()
+#         multiLables = pipeline.getMultiLabelMapping()
+#
+#
+#         return jsonify({
+#             "binary_predictions": binary_predictions.tolist(),
+#             "multi_predictions": multi_predictions.tolist()
+#         })
+#
+#
+#     except KeyError as e:
+#         missing_columns = list(set(e.args[0].split(" ")[-1]) - set(data.columns))
+#         return jsonify({"error": f"Missing required columns: {missing_columns}"}), 400
+#
+#     except Exception as e:
+#         print(f"Error during prediction: {str(e)}")
+#         return jsonify({"error": str(e)}), 500
+#
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
+#
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -91,32 +162,28 @@ def predict():
     try:
         data = pd.read_csv(file)
         print(data.shape)
-        binary, multi= pipeline.fit_test(data)
+        binary, multi = pipeline.fit_test(data)
         print(binary.shape)
         print(multi.shape)
 
-        binary_dmatrix = xgb.DMatrix(binary)
-        binary_predictions = CIC_Binary.predict(binary_dmatrix)
+        binary_predictions = CIC_Binary.predict(binary)
 
         if any(binary_predictions):
             return jsonify({"message": "No attacks occurred"})
 
         attack_indices = [i for i, pred in enumerate(binary_predictions) if pred == 0]
         attack_data = multi.iloc[attack_indices]
-        multi_dmatrix = xgb.DMatrix(attack_data)
-        multi_predictions = CIC_Multi.predict(multi_dmatrix)
+        multi_predictions = CIC_Multi.predict(attack_data)
 
         print(multi_predictions)
 
-        binaryLables = pipeline.getBinaryLabelMapping()
-        multiLables = pipeline.getMultiLabelMapping()
-
+        binaryLabels = pipeline.getBinaryLabelMapping()
+        multiLabels = pipeline.getMultiLabelMapping()
 
         return jsonify({
             "binary_predictions": binary_predictions.tolist(),
             "multi_predictions": multi_predictions.tolist()
         })
-
 
     except KeyError as e:
         missing_columns = list(set(e.args[0].split(" ")[-1]) - set(data.columns))
@@ -126,6 +193,8 @@ def predict():
         print(f"Error during prediction: {str(e)}")
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
